36b0dc7a0c7a170a7ff19ea7a3e28fc53f9c2c34
sibpakorn <ghhh7731@gmail.com>
2022-07-17 15:08:11 +0700

    StatelessWidget1

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  var app = MaterialApp(
-    title: "sibpakorn app",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("hello flutter นะจ้ะ"),
+  var app = runApp(app);
+}
+
+class sibpakornapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    returnMaterialApp(
+      title: "sibpakorn app",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("hello flutter นะจ้ะ"),
+        ),
+        body: Text("สวัสดีครับขอต้อนรับเข้าสู้เเอปดีๆไม่มีไม่ได้เเล้ว"),
       ),
-      body: Text("สวัสดีครับขอต้อนรับเข้าสู้เเอปดีๆไม่มีไม่ได้เเล้ว"),
-    ),
-    theme: ThemeData(primarySwatch: Colors.teal),
-  );
-  runApp(app);
+      theme: ThemeData(primarySwatch: Colors.teal),
+    );
+  }
 }
