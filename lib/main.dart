bb35d1ef5fb7a92d06c5262994ac3b7913da087a
sibpakorn <ghhh7731@gmail.com>
2022-07-17 15:20:38 +0700

    center welcome2

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  var app = runApp(app);
+  runApp(sibpakornapp());
 }
 
+//สร้างwidget
 class sibpakornapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    returnMaterialApp(
+    return MaterialApp(
       title: "sibpakorn app",
       home: Scaffold(
         appBar: AppBar(
           title: Text("hello flutter นะจ้ะ"),
         ),
-        body: Text("สวัสดีครับขอต้อนรับเข้าสู้เเอปดีๆไม่มีไม่ได้เเล้ว"),
+        body: Center(
+          child: Text("สวัสดีครับขอต้อนรับสู่เเอปดีๆไม่มีไม่ได้เเล้ว"),
+        ),
       ),
       theme: ThemeData(primarySwatch: Colors.teal),
     );
