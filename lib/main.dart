a1800c01b00b7eab0b0be046d94bda1dc3648993
sibpakorn <ghhh7731@gmail.com>
2022-07-17 14:06:01 +0700

    hello flutter นะจ้ะ

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
     title: "sibpakorn app",
-    home: Text("hello dart"),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("hello flutter นะจ้ะ"),
+      ),
+    ),
   );
   runApp(app);
 }
