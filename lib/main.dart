cdedf606eab2eb360adc554777bd3cf7f15c1072
sibpakorn <ghhh7731@gmail.com>
2022-07-17 16:38:15 +0700

    widget-myhome

--- lib/main.dart
+++ lib/main.dart
@@ -10,22 +10,34 @@ class sibpakornapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "sibpakorn app",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("hello flutter นะจ้ะ"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("สวัสดีครับขอต้อนรับแอปดีๆไม่มีไม่ได้เเล้ว"),
-              Text("hello flutter"),
-              Text("hellodart"),
-            ],
-          ),
+      home: myhomesibpakorn(),
+      theme: ThemeData(primarySwatch: Colors.purple),
+    );
+  }
+}
+
+class myhomesibpakorn extends StatefulWidget {
+  @override
+  _myhomesibpakornState createState() => _myhomesibpakornState();
+}
+
+class _myhomesibpakornState extends State<myhomesibpakorn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("hello flutter นะจ้ะ"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("สวัสดีครับขอต้อนรับแอปดีๆไม่มีไม่ได้เเล้ว"),
+            Text("hello flutter"),
+            Text("hellodart"),
+          ],
         ),
       ),
-      theme: ThemeData(primarySwatch: Colors.purple),
     );
   }
 }
