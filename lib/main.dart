4f330f965b95bbe3e94365849847f28e2f763527
sibpakorn <ghhh7731@gmail.com>
2022-07-24 20:59:39 +0700

    column(1)

--- lib/main.dart
+++ lib/main.dart
@@ -23,14 +23,6 @@ class myhomesibpakorn extends StatefulWidget {
 }
 
 class _myhomesibpakornState extends State<myhomesibpakorn> {
-//กลุ่มข้อมูล
-  List<equipmentmenu> menu = [
-    equipmentmenu("มีด", "100000", "assets/images/picture1.jpg"),
-    equipmentmenu("ขวาน", "1050000", "assets/images/picture4.jpg"),
-    equipmentmenu("เลื่อยตัดไม้", "405600", "assets/images/picture2.jpg"),
-    equipmentmenu("เลื่อยตัดเหล็ก", "1500000", "assets/images/picture3.jpg"),
-  ];
-
 //เเสดงผลข้อมูล
   @override
   Widget build(BuildContext context) {
@@ -38,21 +30,8 @@ class _myhomesibpakornState extends State<myhomesibpakorn> {
         appBar: AppBar(
           title: Text("เลือกอุปกรณ์"),
         ),
-        body: ListView.builder(
-            itemCount: menu.length,
-            itemBuilder: (BuildContext context, int index) {
-              equipmentmenu equipment = menu[index];
-              return ListTile(
-                leading: Image.asset(equipment.img),
-                title: Text(
-                  equipment.name,
-                  style: TextStyle(fontSize: 30),
-                ),
-                subtitle: Text("ราคา" + equipment.price + "บาท"),
-                onTap: () {
-                  print("คุณเลือกอุปกรณ์ชื่อว่า" + equipment.name);
-                },
-              );
-            }));
+        body: Column(
+          children: [],
+        ));
   }
 }
